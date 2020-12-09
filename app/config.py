9bcaff0a91041e92bff92c3b0ed5cb1bf0d9c650
ezpinsky <ezpinsky@gmail.com>
2020-12-09 18:28:35 -0500

    sets up flask

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 import os
+from os import environ
 
 
 class Configuration:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
