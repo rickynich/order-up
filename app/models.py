863f1cac7960d8ef7f81b9b83d0af9e47711d24e
Nick Richard <nrich414@gmail.com>
2020-12-09 18:46:49 -0500

    Employee mapping class setup

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.types import Integer, String
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+
+class Employee(db.Model, UserMixin):
+    __tablename__ = 'employees'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    employee_number = Column(Integer, nullable=False, unique=True)
+    hashed_password = Column(String(100), nullable=False)
+
+    @property
+    def password(self):
+        return self.hashed_password
+        
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
